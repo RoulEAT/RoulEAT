46c23b15fe82b8fb8ebe57fa4a903becbeaea72f
William Watson <williampwatson1991@gmail.com>
2022-11-09 16:11:13 -0500

    feat: changes made

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path='/' element={<LandingPage />} />
-        <Route path='/wheel' element={<Wheel />} /> */}
+        <Route path='/' element={<LandingPage />} />
+        {/* <Route path='/wheel' element={<Wheel />} /> */}
       </Routes>
     </Router>
   );
---
+++ client/src/components/LandingPage/Location/index.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './styles.css';
+
+const Location = () => {
+  const navigate = useNavigate();
+  const [location, setLocation] = useState<string | undefined>('');
+  const [error, setError] = useState<string | undefined>('');
+
+  const handleSubmit = (address: string | undefined) => {
+    try {
+      setError('');
+      if (!address) {
+        setError('Please Enter an Address.');
+      }
+      //run post request to backend for address search
+      /*
+      PSUEDO CODE!!!!!!
+      const response = await googleMapsFunction(address);
+      if (response) setRestaurants(response);
+      */
+      //navigate to wheel page
+      setError('');
+      navigate('/wheel');
+    } catch (error) {
+      setError(error as string);
+    }
+  };
+
+  return (
+    <>
+      <div className='LocationContainer'>
+        <input
+          className='LocationInput'
+          placeholder='Delivery Address'
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <button
+          className='LocationButton'
+          onClick={() => handleSubmit(location)}
+        >
+          Submit
+        </button>
+      </div>
+      {error && <span className='LocationError'>{error}</span>}
+    </>
+  );
+};
+
+export default Location;
