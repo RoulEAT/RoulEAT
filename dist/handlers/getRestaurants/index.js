318cbbd59233f313bfa18a886fb1c890aba50192
William Watson <williampwatson1991@gmail.com>
2022-11-09 16:44:01 -0500

    feat: yelp api responding properly

--- dist/handlers/getRestaurants/index.js
+++ dist/handlers/getRestaurants/index.js
@@ -1,15 +1,37 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getRestaurants = void 0;
-const getRestaurants = (request, response) => {
+const axios_1 = __importDefault(require("axios"));
+// interface YelpResponse {
+// }
+const getRestaurants = (request, response) => __awaiter(void 0, void 0, void 0, function* () {
     console.log(request.params);
     console.log(request.body);
     console.log(request.query);
+    const { term, location } = request.body;
     try {
-        return;
+        const { data: yelpResponse } = yield axios_1.default.get(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}`, {
+            headers: {
+                Authorization: `Bearer ${process.env.YELP_API_KEY}`,
+            },
+        });
+        console.log(yelpResponse.businesses);
+        return response.status(200).json(yelpResponse.businesses);
     }
     catch (error) {
         console.log(error, 'THIS IS THE ERRROR CHEIF');
     }
-};
+});
 exports.getRestaurants = getRestaurants;
--- handlers/getRestaurants/index.ts
+++ handlers/getRestaurants/index.ts
@@ -1,12 +1,27 @@
 import { Request, Response } from 'express';
 import axios from 'axios';
 
-export const getRestaurants = (request: Request, response: Response) => {
+// interface YelpResponse {
+// }
+
+export const getRestaurants = async (request: Request, response: Response) => {
   console.log(request.params);
   console.log(request.body);
   console.log(request.query);
+  const { term, location } = request.body;
+
   try {
-    return;
+    const {data: yelpResponse} = await axios.get(
+      `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.YELP_API_KEY}`,
+        },
+      }
+    );
+
+    console.log(yelpResponse.businesses);
+    return response.status(200).json(yelpResponse.businesses);
   } catch (error) {
     console.log(error, 'THIS IS THE ERRROR CHEIF');
   }
