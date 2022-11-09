f206f38a57c0545e242d1d7c47d0f78f9b77e3d9
William Watson <williampwatson1991@gmail.com>
2022-11-09 16:16:54 -0500

    feat: app somewhat back to where we were before

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
+import Wheel from './components/Wheel';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<LandingPage />} />
-        {/* <Route path='/wheel' element={<Wheel />} /> */}
+        <Route path='/wheel' element={<Wheel />} />
       </Routes>
     </Router>
   );
