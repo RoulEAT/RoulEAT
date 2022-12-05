3848c9b386781565c4e049b57a9b4179465fd442
William Watson <williampwatson1991@gmail.com>
2022-12-05 15:16:44 -0500

    feat: wheel prototype working well, for the most part. Universal background color shifted to index.css

--- client/src/components/Wheel/index.tsx
+++ client/src/components/Wheel/index.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './styles.css';
 
 const Wheel = () => {
+
+  const [selectedItemIdx, setSelectedItemIdx] = useState<number | null>(null);
+  const [foodCategory, setFoodCategory] = useState<string | null>(null);
+  const [items, setItems] = useState<string[]>([
+    'Pizza',
+    'Burger',
+    'Ramen',
+    'Curry',
+    'Sushi',
+    'Tacos',
+  ]);
+
+  const selectItem = () => {
+    if (selectedItemIdx === null) {
+      const randomIdx = Math.floor(Math.random() * items.length);
+      setSelectedItemIdx(randomIdx);
+      const selectedItem = items[randomIdx];
+      setFoodCategory(selectedItem);
+    } else {
+      setSelectedItemIdx(null);
+    }
+  }
+
+  const spinning = selectedItemIdx !== null ? 'spinning' : '';
+
+  const wheelVars = {
+    '--nb-item': items.length,
+    '--selected-item': selectedItemIdx,
+  } as React.CSSProperties;
+
   return (
-    <div>
-      <h1>Wheel</h1>
+    <div className='wheel-container'>
+      <div className={`wheel ${spinning}`} style={wheelVars} onClick={selectItem}>
+        {items.map((item, index) => (
+          <div
+            className='wheel-item'
+            key={index}
+            style={{ '--item-nb': index } as React.CSSProperties}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
