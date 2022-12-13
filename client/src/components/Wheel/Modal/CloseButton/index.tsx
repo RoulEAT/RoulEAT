af87326d57bce09b26ca0dff2a8e4c57528e4a8d
William Watson <williampwatson1991@gmail.com>
2022-12-13 15:46:59 -0500

    feat: dummy component continued

--- client/src/components/Wheel/Modal/CloseButton/index.tsx
+++ client/src/components/Wheel/Modal/CloseButton/index.tsx
@@ -5,7 +5,9 @@ import { IconContext } from 'react-icons';
 const CloseButton = () => {
   return (
     <div className='CloseButton'>
-      <IconContext.Provider value={{ color: '#efe9f4' }}><GiKnifeFork /></IconContext.Provider>
+      <IconContext.Provider value={{ color: '#efe9f4' }}>
+        <GiKnifeFork />
+      </IconContext.Provider>
     </div>
   );
 };
