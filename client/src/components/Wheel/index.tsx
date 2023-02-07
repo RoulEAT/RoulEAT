87b953eff289013c92cda68a1b893c88f0f3b31d
William Watson <williampwatson1991@gmail.com>
2023-02-07 09:11:33 -0500

    save

--- client/src/components/Wheel/index.tsx
+++ client/src/components/Wheel/index.tsx
@@ -56,6 +56,8 @@ const Wheel = (props: WheelProps) => {
           location: location || 'New York, NY',
         });
 
+        console.log(restaurants);
+
         setRestaurants(restaurants);
         setTimeout(() => {
           setModalOpen(true);
