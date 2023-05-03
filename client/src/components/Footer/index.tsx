99eba4b64f44f58aeee9b14f728130ff98a6a5a9
William Watson <williampwatson1991@gmail.com>
2023-05-03 18:16:10 -0400

    fix: spelling error

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='Footer'>
       <p>
-        ©ROULEAT was designed and developed by{' '}
+        ©RoulEAT was designed and developed by{' '}
         <a
           href='https://www.github.com/will-watson'
           target='_blank'
