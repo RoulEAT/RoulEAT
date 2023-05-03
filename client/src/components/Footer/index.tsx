6e2fcf64e230d92de13e2cca7e1fe02046eb272e
William Watson <williampwatson1991@gmail.com>
2023-05-03 14:45:42 -0400

    feat: footer added only to landing page

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -3,15 +3,23 @@ import './styles.css';
 
 const Footer = () => {
   return (
-    <div className='FooterContainer'>
+    <div className='Footer'>
       <p>
-        Designed and Developed by{' '}
-        <a href='https://www.github.com/will-watson' target='_blank' rel='noreferrer'>William Watson </a>
+        RoulEAT was Designed and Developed by{' '}
+        <a
+          href='https://www.github.com/will-watson'
+          target='_blank'
+          rel='noreferrer'
+        >
+          William Watson
+        </a>{' '}
         and{' '}
-        <a href='https://www.github.com/JCOOK' target='_blank' rel='noreferrer'>Jeremy Cook</a>
+        <a href='https://www.github.com/JCOOK' target='_blank' rel='noreferrer'>
+          Jeremy Cook
+        </a>
       </p>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
