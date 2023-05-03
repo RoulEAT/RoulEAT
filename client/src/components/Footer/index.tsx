3b263b7a43e2f5dd04603a25cbe42616a69b8fb1
William Watson <williampwatson1991@gmail.com>
2023-05-03 14:37:11 -0400

    feat: footer skeleton component created

---
+++ client/src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css';
+
+const Footer = () => {
+  return (
+    <div className='FooterContainer'>
+      <p>
+        Designed and Developed by{' '}
+        <a href='https://www.github.com/will-watson' target='_blank' rel='noreferrer'>William Watson </a>
+        and{' '}
+        <a href='https://www.github.com/JCOOK' target='_blank' rel='noreferrer'>Jeremy Cook</a>
+      </p>
+    </div>
+  )
+}
+
+export default Footer;
